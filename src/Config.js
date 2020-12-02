da01d37161f6036db74dfff9569d1c2ba7f4d39e
HocheolJang <daaan0415@gmail.com>
2020-12-03 03:43:42 +0900

    Add Layout and function (Kakao, Google Login)

--- src/Config.js
+++ src/Config.js
@@ -1,1 +1,4 @@
-export const KAKAO_MAP_API_KEY = 'f57f55b8f364f077b0b1436432c7392e';
+export const KAKAO_MAP_API_KEY = "f57f55b8f364f077b0b1436432c7392e";
+export const MAIN_API_URL = "http://10.58.6.182:8000";
+export const KAKAO_LOGIN_API_URL = `${MAIN_API_URL}/user/kakaologin`;
+export const GOOGLE_LOGIN_API_URL = `${MAIN_API_URL}/user/googlelogin`;
